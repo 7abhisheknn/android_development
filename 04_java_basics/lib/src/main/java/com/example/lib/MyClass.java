26910c4e60a065f1602696ea0d0e7ca57b864bf8
Abhishek N N <7abhisheknn@gmail.com>
2024-02-18 11:58:38 +0530

    014 Conditional Statements

--- 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
+++ 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
@@ -3,17 +3,15 @@ package com.example.lib;
 public class MyClass {
 
     public static void main(String[] args) {
-        // Creating Strings:
-        String sayHello = "Hello ";
-
-        // Printing Strings:
-        System.out.println(sayHello);
-
-        // Concatenating Strings: "+"
-        String myFriends = "My Friends";
-        System.out.println(sayHello + myFriends);
-
-        int length = myFriends.length();
-        System.out.println("The length is: " + length);
+        // Conditional Statements:
+        // if statement
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("You are an adult");
+        } else if (age >= 60) {
+            System.out.println("Aged Person");
+        } else {
+            System.out.println("You are an minor");
+        }
     }
 }
