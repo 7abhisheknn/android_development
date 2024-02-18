b532c93c5190c3daaae94a4125f306f3590d062b
Abhishek N N <7abhisheknn@gmail.com>
2024-02-18 20:35:10 +0530

    002 method return

--- 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
+++ 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
@@ -5,9 +5,18 @@ public class MyClass {
     public static void main(String[] args) {
 
         sayHello();
+        System.out.println(addNumbers());
     }
 
     static void sayHello() {
         System.out.println("Hello world from method");
+    }
+
+    static int addNumbers() {
+        int x = 10;
+        int y = 20;
+
+        int z = x + y;
+        return z;
     }
 }
