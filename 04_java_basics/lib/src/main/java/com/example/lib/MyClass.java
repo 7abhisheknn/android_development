31ee2f1c9c96acba41ea67c96ff274554a2fef36
Abhishek N N <7abhisheknn@gmail.com>
2024-02-18 11:45:33 +0530

    012 Operators

--- 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
+++ 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
@@ -3,24 +3,36 @@ package com.example.lib;
 public class MyClass {
 
     public static void main(String[] args) {
-        // Type Casting: is the process of converting
-        // a value from one data type to another
+        //Arithemetic Operators + - * / %
+        System.out.println(10+3); // 13
+        System.out.println(10-3); // 7
+        System.out.println(10*3); // 30
+        System.out.println(10/3); // 3
+        System.out.println(10%3); // 1
 
-        // Implicit Type Casting
-        int numInt = 10;
-        double numDouble = numInt;
-        System.out.println(numInt);
-        System.out.println(numDouble);
+        // Comparision Operators: == != > < >= <=
+        System.out.println(10 == 10); // true
+        System.out.println(11 == 10); // false
 
-        // Explicit Type Casting
-        double pi = 3.14;
-        int numPi = (int)pi;
-        System.out.println(pi);
-        System.out.println(numPi);
+        System.out.println(10 != 10); // false
+        System.out.println(11 != 10); // true
 
-        // Type Casting between different Data Types
-        int myInt = 5;
-        char myChar = (char)(myInt + 'A');
-        System.out.println(myChar);
+        System.out.println(10 > 10); // false
+        System.out.println(11 > 10); // true
+
+        System.out.println(10 < 10); // false
+        System.out.println(10 < 11); // true
+
+        System.out.println(9 >= 10); // false
+        System.out.println(11 >= 10); // true
+
+        System.out.println(10 <= 10); // true
+        System.out.println(11 <= 10); // false
+
+        // Logical Operators: && || !
+        System.out.println(true && false); // false
+        System.out.println(true || false); // true
+        System.out.println(!true); // false
+
     }
 }
