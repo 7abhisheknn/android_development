13bcf26c09be33211cab4d83337257267e3c933f
Abhishek N N <7abhisheknn@gmail.com>
2024-02-18 11:52:45 +0530

    013 Strings

--- 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
+++ 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
@@ -3,36 +3,17 @@ package com.example.lib;
 public class MyClass {
 
     public static void main(String[] args) {
-        //Arithemetic Operators + - * / %
-        System.out.println(10+3); // 13
-        System.out.println(10-3); // 7
-        System.out.println(10*3); // 30
-        System.out.println(10/3); // 3
-        System.out.println(10%3); // 1
+        // Creating Strings:
+        String sayHello = "Hello ";
 
-        // Comparision Operators: == != > < >= <=
-        System.out.println(10 == 10); // true
-        System.out.println(11 == 10); // false
+        // Printing Strings:
+        System.out.println(sayHello);
 
-        System.out.println(10 != 10); // false
-        System.out.println(11 != 10); // true
+        // Concatenating Strings: "+"
+        String myFriends = "My Friends";
+        System.out.println(sayHello + myFriends);
 
-        System.out.println(10 > 10); // false
-        System.out.println(11 > 10); // true
-
-        System.out.println(10 < 10); // false
-        System.out.println(10 < 11); // true
-
-        System.out.println(9 >= 10); // false
-        System.out.println(11 >= 10); // true
-
-        System.out.println(10 <= 10); // true
-        System.out.println(11 <= 10); // false
-
-        // Logical Operators: && || !
-        System.out.println(true && false); // false
-        System.out.println(true || false); // true
-        System.out.println(!true); // false
-
+        int length = myFriends.length();
+        System.out.println("The length is: " + length);
     }
 }
