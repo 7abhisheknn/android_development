526803625cbeda93b21e304d153521dc55e4d72b
Abhishek N N <7abhisheknn@gmail.com>
2024-02-18 20:29:25 +0530

    001 methods

--- 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
+++ 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
@@ -3,16 +3,11 @@ package com.example.lib;
 public class MyClass {
 
     public static void main(String[] args) {
-        int[] numbersArray = new int[5];
 
-        numbersArray[0] = 10;
-        numbersArray[1] = 20;
-        numbersArray[2] = 30;
-        numbersArray[3] = 40;
-        numbersArray[4] = 50;
+        sayHello();
+    }
 
-        for(int num: numbersArray) {
-            System.out.println(num);
-        }
+    static void sayHello() {
+        System.out.println("Hello world from method");
     }
 }
