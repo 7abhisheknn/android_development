a8df5791415b2695154641dcc9c69b8b063e94cd
Abhishek N N <7abhisheknn@gmail.com>
2024-02-18 20:43:16 +0530

    004 Method Overloading

--- 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
+++ 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
@@ -3,17 +3,15 @@ package com.example.lib;
 public class MyClass {
 
     public static void main(String[] args) {
-
-        sayHello();
         System.out.println(addNumbers(100, 200));
-    }
-
-    static void sayHello() {
-        System.out.println("Hello world from method");
+        System.out.println(addNumbers(100.55, 200.50));
     }
 
 
     static int addNumbers(int x, int y) {
+        return x + y;
+    }
+    static double addNumbers(double x, double y) {
         return x + y;
     }
 }
