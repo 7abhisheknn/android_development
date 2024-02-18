b82d541ef520a0c8609339e67f9e3dcecd7056b2
Abhishek N N <7abhisheknn@gmail.com>
2024-02-18 12:08:19 +0530

    016 While loop

--- 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
+++ 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
@@ -3,22 +3,12 @@ package com.example.lib;
 public class MyClass {
 
     public static void main(String[] args) {
-        // Switch Statement:
+        // While loop
 
-        int dayOfWeek = 3;
-        switch (dayOfWeek) {
-            case 1:
-                System.out.println("Sunday");
-                break;
-            case 2:
-                System.out.println("Monday");
-                break;
-            case 3:
-                System.out.println("Tuesday");
-                break;
-            default:
-                System.out.println("Unknown");
-                break;
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            ++i;
         }
     }
 }
