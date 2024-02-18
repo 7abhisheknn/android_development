a2aca334e3f8da7dbfeae859ae0d4a64dcf19e85
Abhishek N N <7abhisheknn@gmail.com>
2024-02-18 09:42:41 +0530

    first_commit

---
+++ 04_java_basics/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "04_java_basics"
+include(":app")
+include(":lib")
