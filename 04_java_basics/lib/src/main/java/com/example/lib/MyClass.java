3505068525e0e0f16e3011a8b88a3af59a4c2d3c
Abhishek N N <7abhisheknn@gmail.com>
2024-02-18 12:02:20 +0530

    015 Switch Statement

--- 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
+++ 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
@@ -3,15 +3,22 @@ package com.example.lib;
 public class MyClass {
 
     public static void main(String[] args) {
-        // Conditional Statements:
-        // if statement
-        int age = 18;
-        if (age >= 18) {
-            System.out.println("You are an adult");
-        } else if (age >= 60) {
-            System.out.println("Aged Person");
-        } else {
-            System.out.println("You are an minor");
+        // Switch Statement:
+
+        int dayOfWeek = 3;
+        switch (dayOfWeek) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            default:
+                System.out.println("Unknown");
+                break;
         }
     }
 }
