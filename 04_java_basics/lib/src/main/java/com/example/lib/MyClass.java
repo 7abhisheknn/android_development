cd4ad39418f7e0354b219f8c1415c1c9458baa5a
Abhishek N N <7abhisheknn@gmail.com>
2024-02-18 10:19:19 +0530

    007 numbers

--- 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
+++ 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
@@ -3,9 +3,17 @@ package com.example.lib;
 public class MyClass {
 
     public static void main(String[] args) {
+        byte b = 100;
+        System.out.println("Byte " + b + " Max " + Byte.MAX_VALUE + " Min " + Byte.MIN_VALUE);
 
-        int age = 60;
-        age = 20;
-        System.out.print("My age: " + age);
+        short s = 7777;
+        System.out.println("Short " + s + " Max " + Short.MAX_VALUE + " Min " + Short.MIN_VALUE);
+
+        int i = 888777666;
+        System.out.println("Integer " + i + " Max " + Integer.MAX_VALUE + " Min " + Integer.MIN_VALUE);
+
+        long l = 1122334455667788L;
+        System.out.println("Long " + l + " Max " + Long.MAX_VALUE + " Min " + Long.MIN_VALUE);
+
     }
 }
