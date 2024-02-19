8d228fb3ba06aa37b79173fecde2e8fc964b52e3
Abhishek N N <7abhisheknn@gmail.com>
2024-02-19 20:33:06 +0530

    007 E

--- 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
+++ 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
@@ -10,19 +10,15 @@ public class MyClass {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr1 = new int[n];
-        int[] arr2 = new int[n];
-
+        int even = 0;
+        int odd = 0;
         for(int i = 0; i < n; i++) {
-            arr1[i] = sc.nextInt();
+            int x = sc.nextInt();
+            if (x%2 == 0) ++even;
+            else ++odd;
         }
-        for(int i = 0; i < n; i++) {
-            arr2[i] = sc.nextInt();
-        }
-
-        for(int i = 0; i < n; i++) {
-            System.out.print((arr1[i] * arr2[i]) + " ");
-        }
+        System.out.println("even: " + even);
+        System.out.println("odd: " + odd);
     }
 
 }
