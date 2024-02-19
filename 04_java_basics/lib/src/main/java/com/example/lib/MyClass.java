09e3e1de810e48a9e6f0d7ae2e267e06ff14793b
Abhishek N N <7abhisheknn@gmail.com>
2024-02-19 20:22:29 +0530

    005 E

--- 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
+++ 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
@@ -10,36 +10,10 @@ public class MyClass {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        int upper = 0;
-        int lower = 0;
-        int number = 0;
-        int space = 0;
-        int special = 0;
-        char[] charArr = s.toCharArray();
-        for(char c: charArr) {
-            switch (Character.getType(c)) {
-                case Character.UPPERCASE_LETTER:
-                    ++upper;
-                    break;
-                case Character.LOWERCASE_LETTER:
-                    ++lower;
-                    break;
-                case Character.DECIMAL_DIGIT_NUMBER:
-                    ++number;
-                    break;
-                case Character.SPACE_SEPARATOR:
-                    ++space;
-                    break;
-                default:
-                    ++special;
-                    break;
-            }
-        }
-        System.out.println("upper case: " + upper);
-        System.out.println("lower case: " + lower);
-        System.out.println("    number: " + number);
-        System.out.println("     space: " + space);
-        System.out.println("   special: " + special);
+        System.out.println(reverse(s));
+    }
+    public static String reverse(String s) {
+        return (new StringBuilder(s)).reverse().toString();
     }
 }
 
