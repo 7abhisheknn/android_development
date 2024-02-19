f23c4611edc2e000568f99de52fb35b39a02de0d
Abhishek N N <7abhisheknn@gmail.com>
2024-02-19 17:04:29 +0530

    003 E

--- 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
+++ 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
@@ -1,17 +1,17 @@
 package com.example.lib;
 
-import java.util.Scanner;
 
 public class MyClass {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Give radius: ");
-        double r = sc.nextDouble();
-        Circle c = new Circle(r);
-        System.out.println(c.getArea());
-        System.out.println(c.getPerimeter());
-        System.out.println(c.getPerimeter());
+        int n = 6;
+        StringBuilder sb = new StringBuilder();
+        while (n != 0) {
+            int rem = n % 2;
+            n /= 2;
+            sb.append(rem);
+        }
+        System.out.println(sb.reverse());
     }
 }
 
