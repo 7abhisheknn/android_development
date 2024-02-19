9d0dcfe88718b056b77b80077f6af5ccb4d68aff
Abhishek N N <7abhisheknn@gmail.com>
2024-02-19 21:51:19 +0530

    010 E

--- 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
+++ 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
@@ -9,27 +9,12 @@ public class MyClass {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int r = sc.nextInt();
-        int c = sc.nextInt();
-        int[][] mat1 = new int[r][c];
-        int[][] mat2 = new int[r][c];
-
-        for(int i = 0; i < r; ++i) {
-            for(int j = 0; j < c; ++j) {
-                mat1[i][j] = sc.nextInt();
-            }
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            sum += sc.nextInt();
         }
-        for(int i = 0; i < r; ++i) {
-            for(int j = 0; j < c; ++j) {
-                mat2[i][j] = sc.nextInt();
-            }
-        }
-        for(int i = 0; i < r; ++i) {
-            for(int j = 0; j < c; ++j) {
-                System.out.print((mat1[i][j] + mat2[i][j]) + " ");
-            }
-            System.out.println();
-        }
+        System.out.println("Avg: " + (float)sum/n);
 
     }
 
