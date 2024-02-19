37c6ba01451e027ea690f4d2e68fef7e2bac2259
Abhishek N N <7abhisheknn@gmail.com>
2024-02-19 20:16:40 +0530

    004 E

--- 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
+++ 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
@@ -1,17 +1,45 @@
 package com.example.lib;
 
 
+import com.sun.org.apache.xpath.internal.operations.Neg;
+
+import java.util.Scanner;
+
 public class MyClass {
 
     public static void main(String[] args) {
-        int n = 6;
-        StringBuilder sb = new StringBuilder();
-        while (n != 0) {
-            int rem = n % 2;
-            n /= 2;
-            sb.append(rem);
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int upper = 0;
+        int lower = 0;
+        int number = 0;
+        int space = 0;
+        int special = 0;
+        char[] charArr = s.toCharArray();
+        for(char c: charArr) {
+            switch (Character.getType(c)) {
+                case Character.UPPERCASE_LETTER:
+                    ++upper;
+                    break;
+                case Character.LOWERCASE_LETTER:
+                    ++lower;
+                    break;
+                case Character.DECIMAL_DIGIT_NUMBER:
+                    ++number;
+                    break;
+                case Character.SPACE_SEPARATOR:
+                    ++space;
+                    break;
+                default:
+                    ++special;
+                    break;
+            }
         }
-        System.out.println(sb.reverse());
+        System.out.println("upper case: " + upper);
+        System.out.println("lower case: " + lower);
+        System.out.println("    number: " + number);
+        System.out.println("     space: " + space);
+        System.out.println("   special: " + special);
     }
 }
 
