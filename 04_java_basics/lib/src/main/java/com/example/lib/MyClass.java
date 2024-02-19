dc26e6664f1a752430025869303409577878480a
Abhishek N N <7abhisheknn@gmail.com>
2024-02-19 20:28:06 +0530

    006 E

--- 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
+++ 04_java_basics/lib/src/main/java/com/example/lib/MyClass.java
@@ -9,11 +9,21 @@ public class MyClass {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        System.out.println(reverse(s));
+        int n = sc.nextInt();
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        for(int i = 0; i < n; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n; i++) {
+            System.out.print((arr1[i] * arr2[i]) + " ");
+        }
     }
-    public static String reverse(String s) {
-        return (new StringBuilder(s)).reverse().toString();
-    }
+
 }
 
