52eb3a87d89b72cbe5843a0c66604b17ec60e082
Abhishek N N <7abhisheknn@gmail.com>
2024-02-20 11:30:06 +0530

    008 Listeners in android

--- 07_views_and_widgets/app/src/main/java/com/example/d07_views_and_widgets/MainActivity.java
+++ 07_views_and_widgets/app/src/main/java/com/example/d07_views_and_widgets/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         myBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Toast.makeText(MainActivity.this, "You Clicked the Button", Toast.LENGTH_SHORT).show();
             }
         });
 
