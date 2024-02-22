3c3676614106f45237639b6648d7bc41c2824933
Abhishek N N <7abhisheknn@gmail.com>
2024-02-22 21:29:33 +0530

    004 menu

--- 12_app_resources/app/src/main/java/com/example/a12_app_resources/MainActivity.java
+++ 12_app_resources/app/src/main/java/com/example/a12_app_resources/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.a12_app_resources;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -21,5 +24,26 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Button clicked", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.action_home) {
+            Toast.makeText(this, "home menu clicked", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.action_search) {
+            Toast.makeText(this, "search menu clicked", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "diff menu clicked", Toast.LENGTH_SHORT).show();
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
