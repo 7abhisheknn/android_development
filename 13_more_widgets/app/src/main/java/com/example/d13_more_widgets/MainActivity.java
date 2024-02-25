b2aa7500fd66833495c8f1923acce840dbbd1cfe
Abhishek N N <7abhisheknn@gmail.com>
2024-02-25 10:53:58 +0530

    001 checkbox

--- 13_more_widgets/app/src/main/java/com/example/d13_more_widgets/MainActivity.java
+++ 13_more_widgets/app/src/main/java/com/example/d13_more_widgets/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.d13_more_widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    CheckBox checkbox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkbox = findViewById(R.id.checkbox);
+
+        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(MainActivity.this, "Checkbox is checked", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Checkbox is not checked", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
