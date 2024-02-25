25bdf0f7c48d2369a2bba21da8b0c99e223db970
Abhishek N N <7abhisheknn@gmail.com>
2024-02-25 11:04:49 +0530

    002 radio button

--- 13_more_widgets/app/src/main/java/com/example/d13_more_widgets/MainActivity.java
+++ 13_more_widgets/app/src/main/java/com/example/d13_more_widgets/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     CheckBox checkbox;
+    RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     Toast.makeText(MainActivity.this, "Checkbox is not checked", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = findViewById(checkedId);
+                Toast.makeText(MainActivity.this, radioButton.getText().toString() + " this radio selected", Toast.LENGTH_SHORT).show();
             }
         });
     }
