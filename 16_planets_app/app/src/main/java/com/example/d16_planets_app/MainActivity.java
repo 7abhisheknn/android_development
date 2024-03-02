272d477a97900bf9f3877008fa52800a6a487772
Abhishek N N <7abhisheknn@gmail.com>
2024-03-02 22:32:12 +0530

    project complete

--- 16_planets_app/app/src/main/java/com/example/d16_planets_app/MainActivity.java
+++ 16_planets_app/app/src/main/java/com/example/d16_planets_app/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.d16_planets_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -42,5 +45,12 @@ public class MainActivity extends AppCompatActivity {
         myCustomAdapter = new MyCustomAdapter(planetArrayList, getApplicationContext());
 
         listView.setAdapter(myCustomAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Planet Name: " + myCustomAdapter.getItem(position).getPlanetName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
