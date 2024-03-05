bc975bbc2dc53c0283124185aa0fdb4b84879f71
Abhishek N N <7abhisheknn@gmail.com>
2024-03-05 12:02:12 +0530

    003_pdfviewer

--- 19_libraries_and_dependencies/app/src/main/java/com/example/d19_libraries_and_dependencies/MainActivity.java
+++ 19_libraries_and_dependencies/app/src/main/java/com/example/d19_libraries_and_dependencies/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.d19_libraries_and_dependencies;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.github.barteksc.pdfviewer.PDFView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +24,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ImageView imageView = findViewById(R.id.imageView);
+//        imageView.setImageResource(R.drawable.test_img);
+        Glide.with(this).load(R.drawable.test_img).into(imageView);
+
+        PDFView pdfView = findViewById(R.id.pdfView);
+
+        pdfView.fromAsset("cap.pdf").load();
     }
 }
