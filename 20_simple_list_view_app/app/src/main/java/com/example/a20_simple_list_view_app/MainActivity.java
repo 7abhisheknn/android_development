e204be1b3a36a00a4ab511f666eac5d56a4791d9
Abhishek N N <7abhisheknn@gmail.com>
2024-03-07 22:54:30 +0530

    world cup started

---
+++ 20_simple_list_view_app/app/src/main/java/com/example/a20_simple_list_view_app/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.a20_simple_list_view_app;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    String[] wordCup2k24 = {"Germany", "Spain", "Brazil", "England", "USA", "Saudi Arabia", "Qatar"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        listView = findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                R.layout.my_list_item,
+                R.id.textView,
+                wordCup2k24
+        );
+
+        listView.setAdapter(adapter);
+    }
+}
---
+++ 20_world_cup_app/app/src/main/java/com/example/a20_world_cup_app/CountryModelClass.java
@@ -0,0 +1,4 @@
+package com.example.a20_world_cup_app;
+
+public class CountryModelClass {
+}
