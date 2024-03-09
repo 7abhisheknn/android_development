6f9643af6d81842788e04ff44341fbb7edd80348
Abhishek N N <7abhisheknn@gmail.com>
2024-03-09 17:04:13 +0530

    complete

--- 21_vaccines_app/app/src/main/java/com/example/a21_vaccines_app/MainActivity.java
+++ 21_vaccines_app/app/src/main/java/com/example/a21_vaccines_app/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.a21_vaccines_app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ItemClickListener{
+    RecyclerView recyclerView;
+    VaccineModel[] listData = {
+            new VaccineModel("Hepatitis B Vaccine", R.drawable.vaccine1),
+            new VaccineModel("Tetanus Vaccine", R.drawable.vaccine4),
+            new VaccineModel("Pneumococcal Vaccine", R.drawable.vaccine5),
+            new VaccineModel("Rotavirus Vaccine", R.drawable.vaccine6),
+            new VaccineModel("Measles Virus", R.drawable.vaccine7),
+            new VaccineModel("Cholera Virus", R.drawable.vaccine8),
+            new VaccineModel("Covid-19 Virus", R.drawable.vaccine9),
+    };
+    VaccineAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recyclerView = findViewById(R.id.recylerView);
+        adapter = new VaccineAdapter(listData);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        adapter.setClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view, int pos) {
+        Toast.makeText(this, "Vaccine Name: " + listData[pos].getTitle(), Toast.LENGTH_SHORT).show();
     }
 }
--- 21_vaccines_app/app/src/main/java/com/example/a21_vaccines_app/VaccineAdapter.java
+++ 21_vaccines_app/app/src/main/java/com/example/a21_vaccines_app/VaccineAdapter.java
@@ -10,19 +10,32 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class VaccineAdapter extends RecyclerView.Adapter<VaccineAdapter.VaccineHolder> {
+    private ItemClickListener clickListener;
     private VaccineModel[] listData;
 
     public VaccineAdapter(VaccineModel[] listData) {
         this.listData = listData;
     }
 
-    public static class VaccineHolder extends RecyclerView.ViewHolder {
+    public void setClickListener(ItemClickListener clickListener) {
+        this.clickListener = clickListener;
+    }
+
+    public class VaccineHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         public ImageView imageView;
         public TextView textView;
         public VaccineHolder(@NonNull View itemView) {
             super(itemView);
             this.imageView = itemView.findViewById(R.id.imageView);
             this.textView = itemView.findViewById(R.id.textView);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (clickListener != null) {
+                clickListener.onClick(v, getAdapterPosition());
+            }
         }
     }
 
